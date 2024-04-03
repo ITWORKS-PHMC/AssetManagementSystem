c4fdeef1c2c02e32a533176367491db1ef1a2659
it.training01 <it.training01@uphmc.com.ph>
2024-04-03 15:00:26 +0800

    First commit

---
+++ dashboard.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asset Management-Dashboard</title>
+    <link rel="stylesheet" href="dashboard.css">
+</head>
+<body>
+<div class="navigation-bar">
+  <div class="header-content">
+    <div class="header-title">
+      <p>PREVENTIVE MAINTENANCE</p>
+    </div>
+    <div class="header-sections">
+      <a href="#" class="nav-link">Dashboard</a>
+      <a href="#" class="nav-link">Scheduling</a>
+      <a href="#" class="nav-link">Files</a>
+      <a href="#" class="nav-link">Ticketing</a>
+      <a href="#" class="nav-link">History</a>
+      <div class="user">
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    <!--- TABLE ----->
+    <div class="table-wrapper">
+    <Table class="table-design">
+    <thead class="table-header">
+    <th scope="col">Serial no.</th>
+    <th scope="col">Equipment name</th>
+    <th scope="col">Brand</th>
+    <th scope="col">Model</th>
+    <th scope="col">Location</th>
+    <th scope="col">Purchase date</th>
+    <th scope="col">Place in service date</th>
+    <th scope="col">Status</th>
+    </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Nitro 5</td>
+                <td>Acer</td>
+                <td>AN515-1234</td>
+                <td>SM Southmall,Zapote,Las Piñas</td>
+                <td>09/17/2020</td>
+                <td>10/20/2020</td>
+                <td>In Progress</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Nitro 6</td>
+                <td>Acer</td>
+                <td>AN515-5678</td>
+                <td>SM Southmall,Zapote,Las Piñas</td>
+                <td>09/10/2020</td>
+                <td>09/15/2020</td>
+                <td>Outdated</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Nitro 7</td>
+                <td>Acer</td>
+                <td>AN515-91011</td>
+                <td>SM Southmall,Zapote,Las Piñas</td>
+                <td>09/20/2020</td>
+                <td>11/15/2020</td>
+                <td>Working</td>
+            </tr>
+        </tbody>
+    </Table>
+    </div>
+</body>
+</html>
