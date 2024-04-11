8b062c1f23a7a28794202452f42e9a53869a050c
kkoze <edcezarlampitoc@gmail.com>
2024-04-11 13:41:47 +0800

    added module for history

---
+++ history.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asset Management - History</title>
+    <link rel="stylesheet" href="style.css">
+    <?php include("./layouts/navbar.php")?>
+</head>
+<body>
+    <h1>History to </h1>
+    <br><hr>
+    <br>
+</body>
+</html>
