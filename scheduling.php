a2826f75f64e125df8b6ee477d7a07b180150f61
kkoze <edcezarlampitoc@gmail.com>
2024-04-11 13:40:16 +0800

    added module for scheduling

---
+++ scheduling.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asset Management - Scheduling</title>
+    <?php include("./layouts/navbar.php")?>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="./lib/calendar.css">
+    <link rel="stylesheet" href="./lib/main.min.css">
+    <script src="./lib/js/jquery-3.6.0.min.js"></script>
+    <script src="./lib/js/bootstrap.min.js"></script>
+    <script src="./lib/main.min.js"></script>
+</head>
+ <style>
+        :root {
+            --bs-success-rgb: 71, 222, 152 !important;
+        }
+
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+          
+        }
+
+        .btn-info.text-light:hover,
+        .btn-info.text-light:focus {
+            background: #000;
+        }
+        table, tbody, td, tfoot, th, thead, tr {
+            border-color: #ededed !important;
+            border-style: solid;
+            border-width: 1px !important;
+        }
+    </style>
+<body>
+    <h1>Scheduling for Maintenance Tasks</h1>
+    <br><hr>
+    <br>
+<!---- CALENDAR ----->
+
+</body>
+</html>
