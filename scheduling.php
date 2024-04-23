8618ab00c2c4cacfe3b0aa1f1ce7d39f4c476cbb
kkoze <edcezarlampitoc@gmail.com>
2024-04-23 17:47:43 +0800

    updated scheduling module with calendar modal

--- scheduling.php
+++ scheduling.php
@@ -15,8 +15,28 @@ require 'function/connect.php';
     <br><hr>
     <br>
 <!---- CALENDAR ----->
+ <div class="button-container">
+            <button class="add-ticket" onclick="openForm()">+ Schedule</button>
+        </div>
 <div class="calendar-container">
-
+<?php
+    include ("scheduling-calendar.php");
+?>
 </div>
+<!-- CALENDAR MODAL for inserting schedule dates -->
+ <div class="Modal" id="pop-up">
+        <div class="popup">
+            <form class="Form" method="post" id="addTicketForm" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <label for="title">Title:</label><br>
+                <input type="text" id="title" name="title"><br>
+                <input type="datetime-local" id="startDate" name="startDate"><br>
+                <label for="enddate">End-Date:</label><br>
+                <input type="datetime-local" id="endDate" name="endDate"><br><br>
+                <input type="hidden" name="addTicket" value="1"> <!-- Add this line -->
+                <input type="submit" value="Submit" class="btn-add">
+                <button type="button" class="btn-cancel" onclick="closeForm()">Close</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
